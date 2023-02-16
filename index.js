c74cdb426b3592fd54ea5849ee08ecd08b444556
Kevin Läufer <laeufer@cs.berkeley.edu>
2023-02-15 21:41:10 -0500

    switch templates

--- index.js
+++
@@ -1,7 +0,0 @@
-// code to calculate what year of my PhD I am in
-document.addEventListener("DOMContentLoaded", function () {
-  start = 1502780400
-  delta_seconds = (Date.now() / 1000) - start
-  delta_years = Math.ceil(delta_seconds / 60 / 60 / 24 / 365)
-  document.getElementById("phd-year").innerHTML = delta_years + "th";
-});
